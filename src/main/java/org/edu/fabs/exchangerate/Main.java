a5cc9695f4f964340ba69f051a05a10425f694c1
bianavic <bianavic@gmail.com>
2023-11-12 10:34:51 -0300

    fix: cors

--- src/main/java/org/edu/fabs/exchangerate/Main.java
+++ src/main/java/org/edu/fabs/exchangerate/Main.java
@@ -1,9 +1,11 @@
 package org.edu.fabs.exchangerate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition
 @EnableFeignClients
 @SpringBootApplication
 public class Main {
